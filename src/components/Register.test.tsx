5e355fba240d696e106963d5100c77e44ae6f895
Michal <michal.p.zarzycki@wp.pl>
2021-06-10 17:32:37 +0200

    Added test to Register component

---
+++ src/components/Register.test.tsx
@@ -0,0 +1,7 @@
+import Register from './Register';
+import renderer from 'react-test-renderer';
+
+it('render correctly Register component', () => {  
+    const RegisterComponent = renderer.create(<Register />).toJSON();
+    expect(RegisterComponent).toMatchSnapshot();
+});
