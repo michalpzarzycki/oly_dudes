e3b7b12a3443fe2d2f0f0701f65b2d39cb779166
Michal <michal.p.zarzycki@wp.pl>
2021-06-14 16:51:35 +0200

    Modified Dashboard

--- src/routers/SignedInRouter.tsx
+++ src/routers/SignedInRouter.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Dashboard from '../components/dashboard/Dashboard';
+import {Dashboard} from '../components/dashboard/Dashboard';
 
 interface SignedInRouterProps {
 
